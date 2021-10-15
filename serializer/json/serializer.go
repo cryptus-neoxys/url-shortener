7852f157e4b0d5853b60cb9b6bcbee6784289629
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-15 20:59:59 +0530

    ✨ feat: add serialzer ports

---
+++ serializer/json/serializer.go
@@ -0,0 +1,26 @@
+package json
+
+import (
+	"encoding/json"
+
+	"github.com/cryptus-neoxys/url-shortener/shortener"
+	"github.com/pkg/errors"
+)
+
+type Redirect struct{}
+
+func (r *Redirect) Decode(input []byte) (*shortener.Redirect, error) {
+	redirect := &shortener.Redirect{}
+	if err := json.Unmarshal(input, redirect); err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Decode")
+	}
+	return redirect, nil
+}
+
+func (r *Redirect) Encode(input *shortener.Redirect) ([]byte, error) {
+	rawMsg, err := json.Marshal(input)
+	if err != nil {
+		return nil, errors.Wrap(err, "serializer.Redirect.Encode")
+	}
+	return rawMsg, nil
+}
