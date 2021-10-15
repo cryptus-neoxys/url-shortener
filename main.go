7b44a030b73d9b39771cf9d6d0ea835c69691981
cryptus-neoxys <sharma.dev4242@gmail.com>
2021-10-16 04:38:05 +0530

    ✨ feat: complete URL shortener REST service

--- main.go
+++ main.go
@@ -1,5 +1,89 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"strconv"
+	"syscall"
 
+	h "github.com/cryptus-neoxys/url-shortener/api"
+	mr "github.com/cryptus-neoxys/url-shortener/repository/mongodb"
+	rr "github.com/cryptus-neoxys/url-shortener/repository/redis"
+	"github.com/cryptus-neoxys/url-shortener/shortener"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/joho/godotenv"
+)
+
+// how the components work together
+// repo <- service -> serializer -> http
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	repo := chooseRepo()
+	if repo == nil {
+		log.Fatal("Couldn't load Repo")
+	}
+	service := shortener.NewRedirectService(repo)
+	handler := h.NewHandler(service)
+
+	r := chi.NewRouter()
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{code}", handler.Get)
+	r.Post("/", handler.Post)
+
+	errs := make(chan error, 2)
+	go func() {
+		fmt.Printf("Listening on port %s\n", httpPort())
+		errs <- http.ListenAndServe(httpPort(), r)
+	}()
+
+	go func() {
+		c := make(chan os.Signal, 1)
+		signal.Notify(c, syscall.SIGINT)
+		errs <- fmt.Errorf("%c", <-c)
+	}()
+
+	fmt.Printf("terminated %s", <-errs)
+}
+
+func httpPort() string {
+	port := "8080"
+	if os.Getenv("PORT") != "" {
+		port = os.Getenv("PORT")
+	}
+	return fmt.Sprintf(":%s", port)
+}
+
+func chooseRepo() shortener.RedirectRepository {
+	switch os.Getenv("URL_DB") {
+	case "redis":
+		redisURL := os.Getenv("REDIS_URL")
+		repo, err := rr.NewRedisRepository(redisURL)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	case "mongo":
+		mongoURL := os.Getenv("MONGO_URL")
+		mongodb := os.Getenv("MONGO_DB")
+		mongoTimeout, _ := strconv.Atoi(os.Getenv("MONGO_TIMEOUT"))
+		repo, err := mr.NewMongoRepository(mongoURL, mongodb, mongoTimeout)
+		if err != nil {
+			log.Fatal(err)
+		}
+		return repo
+	}
+	return nil
 }
